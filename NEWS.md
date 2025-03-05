254028c599ff0a21fb8b7fb481df73f13aa5aff6
Karl Broman <kbroman@gmail.com>
2025-03-05 10:45:41 -0600

    Add map arg to reorg_counts, to save just specified positions

--- NEWS.md
+++ NEWS.md
@@ -1,4 +1,4 @@
-## xoiGBS 0.1.2 (2025-02-13)
+## xoiGBS 0.1.3 (2025-03-05)
 
 - A new package, to analyze recombination and crossover interference
   from genotyping-by-sequencing (GBS) data on a backcross.
--- R/reorg_counts.R
+++ R/reorg_counts.R
@@ -5,6 +5,10 @@
 #' @param counts A list of data frames, one per individual, with six columns: chromosome, basepairs
 #' position, allele1, allele2, readcount1, and readcount 2, as read with `read_counts()`.
 #'
+#' @param map Optional map of SNPs to consider in the output, as a
+#' list of vectors of marker positions. If provided, any other
+#' positions will be ignored.
+#'
 #' @param clean_chr If TRUE, remove "chr" from the chromosome names, so chr13 becomes just 13
 #'
 #' @param quiet If FALSE, print some tracing info
@@ -14,7 +18,7 @@
 #' @export
 
 reorg_counts <-
-    function(counts, clean_chr=TRUE, quiet=TRUE)
+    function(counts, map=NULL, clean_chr=TRUE, quiet=TRUE)
 {
     # set of unique chromosomes
     chr <- unique(unlist(lapply(counts, function(a) unique(a[,1]))))
@@ -22,11 +26,35 @@ reorg_counts <-
     # clean version of those names
     if(clean_chr) cchr <- sub("^chr", "", chr) else cchr <- chr
 
+    if(!is.null(map)) { # omit other chromosomes; pad with further chromosomes
+        if(!all(cchr %in% names(map))) {
+            chr <- chr[cchr %in% names(map)]
+            cchr <- cchr[cchr %in% names(map)]
+        }
+        if(!all(names(map) %in% cchr)) {
+            chr2add <- names(map)[!(names(map) %in% cchr)]
+            chr <- c(chr, chr2add)
+            cchr <- c(cchr, chr2add)
+
+            # reorder
+            chr <- chr[match(cchr, names(map))]
+            cchr <- names(map)
+        }
+    }
+
+
     ind <- names(counts)
     alleles <- sub("Count_", "", colnames(counts[[1]])[5:6])
 
-    map <- output_counts <- vector("list", length(chr))
-    names(map) <- names(output_counts) <- chr
+    if(is.null(map)) {
+        map <- output_counts <- vector("list", length(chr))
+        names(map) <- names(output_counts) <- chr
+    } else { # temporarily use ugly names
+        names(map) <- chr
+
+        # also, change Mbp -> bp
+        for(i in seq_along(map)) map[[i]] <- map[[i]]*1e6
+    }
 
     # loop over chromosomes
     for(this_chr in chr) {
@@ -38,6 +66,11 @@ reorg_counts <-
         # unique positions
         pos <- sort(unique(unlist(lapply(subcounts, "[[", 2))))
 
+        # drop those that aren't in map
+        if(!is.null(map[[this_chr]])) {
+            pos <- pos[pos %in% map[[this_chr]]]
+        }
+
         map[[this_chr]] <- pos/1e6
 
         # object to contain the counts
@@ -46,8 +79,15 @@ reorg_counts <-
 
         # fill in the counts
         for(i in ind) {
-            output_counts[[this_chr]][as.character(subcounts[[i]][,2]) , i, 1] <- subcounts[[i]][,5]
-            output_counts[[this_chr]][as.character(subcounts[[i]][,2]) , i, 2] <- subcounts[[i]][,6]
+            # drop positions not in map
+            rownames(subcounts[[i]]) <- as.character(subcounts[[i]][,2])
+            this_pos <- subcounts[[i]][,2]
+            this_pos <- this_pos[this_pos %in% pos]
+            this_pos <- as.character(this_pos)
+
+            # fill in data
+            output_counts[[this_chr]][this_pos, i, 1] <- subcounts[[i]][this_pos,5]
+            output_counts[[this_chr]][this_pos, i, 2] <- subcounts[[i]][this_pos,6]
         }
 
     }
