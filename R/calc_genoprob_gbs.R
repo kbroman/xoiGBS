4c7064162c3aa520e5e282be96d60c7c28fb2154
Karl Broman <kbroman@gmail.com>
2025-11-05 15:06:37 -0500

    R/calc_genoprob_gbs.R: add comments about what needs to be done

--- R/calc_genoprob_gbs.R
+++ R/calc_genoprob_gbs.R
@@ -4,12 +4,21 @@
 
 calc_genoprob_gbs <-
     function(counts, map, error_prob1=0.002, error_prob2=0.002,
-             map_function)
+             map_function, cores=1)
 {
 
+#    - make sure stuff is the right length
+#
+#    - omit markers that aren't in the map
+#
+#    - fill in 0 counts for pseudomarkers
+#
+#    - turn map distances into rec fracs
+#
+#    - using multiple cores
 
+# pr <- .calc_genoprob_gbs(countsA, countsB, rec_frac, error_prob1, error_prob2)
 
-
-
+#    - just return prob(het) as a matrix
 
 }
