0dfb0915ca6d80db506741fb99a1c597c8254203
Karl Broman <kbroman@gmail.com>
2025-12-12 12:01:42 -0600

    sort_ids and order_ids: also sort by the initial non-numeric part of the ID
    - And add details and examples to the help

--- R/sort_ids.R
+++ R/sort_ids.R
@@ -9,7 +9,21 @@
 #'
 #' @return Input IDs, sorted numerically
 #'
+#' @details We assume the ids have an initial non-numerial label,
+#'     followed by a number, followed possibly by _dup and then
+#'     another number. The IDs are sorted first by the initial
+#'     non-numeric bit, then by the number, then by the duplicate
+#'     number (with absense of `_dup` taken to be duplicate "0" and
+#'     _dup without a number taken to be duplicate "1").
+#'
 #' @seealso `order_ids()`
+#'
+#' @examples
+#' ids <- c("BCA70", "BCA1", "PWD1", "PWD2", "BCA2", "BCA75",
+#'          "BCA70_dup", "PWD1_dup2", "PWD1_dup1")
+#' order_ids(ids)
+#' sort_ids(ids)
+#' sort_ids(ids, decreasing=TRUE)
 #'
 #' @export
 
@@ -31,13 +45,30 @@ sort_ids <-
 #'
 #' @return Input IDs, sorted numerically
 #'
+#' @details We assume the ids have an initial non-numerial label,
+#'     followed by a number, followed possibly by _dup and then
+#'     another number. The IDs are sorted first by the initial
+#'     non-numeric bit, then by the number, then by the duplicate
+#'     number (with absense of `_dup` taken to be duplicate "0" and
+#'     _dup without a number taken to be duplicate "1").
+#'
+#'
 #' @seealso `sort_ids()`
+#'
+#' @examples
+#' ids <- c("BCA70", "BCA1", "PWD1", "PWD2", "BCA2", "BCA75",
+#'          "BCA70_dup", "PWD1_dup2", "PWD1_dup1")
+#' order_ids(ids)
+#' sort_ids(ids)
+#' sort_ids(ids, decreasing=TRUE)
 #'
 #' @export
 
 order_ids <-
     function(ids, decreasing=FALSE)
 {
+    initial_bit <- sapply(strsplit(ids, "[0-9]+"), "[", 1)
+
     dup <- rep(0, length(ids))
     if(any(grepl("_dup", ids))) {
         spl <- strsplit(ids, "_dup")
@@ -56,5 +87,5 @@ order_ids <-
     num_id <- sub("\\-", ".", num_id)
     num_id <- as.numeric(num_id)
 
-    order(num_id, dup, decreasing=decreasing)
+    order(initial_bit, num_id, dup, decreasing=decreasing)
 }
